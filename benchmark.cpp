058c3893a6abed43b1be498c0eadc7362d9fb35c
KRM7 <70973547+KRM7@users.noreply.github.com>
2021-08-13 21:47:37 +0200

    tabs -> spaces

--- benchmark.cpp
+++ benchmark.cpp
@@ -12,47 +12,47 @@ using namespace xorshift;
 template<typename PRNG>
 double benchmarkPRNG(size_t num_rands = 50'000'000)
 {
-	PRNG gen{ random_device{}() };
-	uniform_real_distribution<double> dist(0.0, 100.0);
-	vector<double> numbers(num_rands);
+    PRNG gen{ random_device{}() };
+    uniform_real_distribution<double> dist(0.0, 100.0);
+    vector<double> numbers(num_rands);
 
-	double running_mean_time = 0.0;
-	/* Run 5 times and take the average time. */
-	for (size_t i = 1; i <= 5; i++)
-	{
-		auto tbegin = chrono::high_resolution_clock::now();
-		generate(numbers.begin(), numbers.end(),
-		[&gen, &dist]()
-		{
-			return dist(gen);
-		});
-		auto tend = chrono::high_resolution_clock::now();
+    double running_mean_time = 0.0;
+    /* Run 5 times and take the average time. */
+    for (size_t i = 1; i <= 5; i++)
+    {
+        auto tbegin = chrono::high_resolution_clock::now();
+        generate(numbers.begin(), numbers.end(),
+        [&gen, &dist]()
+        {
+            return dist(gen);
+        });
+        auto tend = chrono::high_resolution_clock::now();
 
-		auto duration = chrono::duration_cast<chrono::microseconds>(tend - tbegin).count();
-		double t = double(duration) / 1E+6;
-		running_mean_time += (t - running_mean_time) / i;
-	}
+        auto duration = chrono::duration_cast<chrono::microseconds>(tend - tbegin).count();
+        double t = double(duration) / 1E+6;
+        running_mean_time += (t - running_mean_time) / i;
+    }
 
-	return running_mean_time;
+    return running_mean_time;
 }
 
 int main()
 {
-	ofstream file;
-	file.open("results.txt");
-	size_t num_passes = 3;
-	for (size_t i = 0; i < num_passes; i++)
-	{
-		file << "Pass " << i << "\n";
-		file << "mersenne:\t" << benchmarkPRNG<mt19937>() << " s\n";
-		file << "mersenne64:\t" << benchmarkPRNG<mt19937_64>() << " s\n";
-		file << "xoroshiro128p:\t" << benchmarkPRNG<xoroshiro128p>() << " s\n";
-		file << "xoshiro256p:\t" << benchmarkPRNG<xoshiro256p>() << " s\n";
-		file << "xoshiro128p:\t" << benchmarkPRNG<xoshiro128p>() << " s\n";
-		file << "xoshiro256ss:\t" << benchmarkPRNG<xoshiro256ss>() << " s\n";
-		file << "xoshiro128ss:\t" << benchmarkPRNG<xoshiro128ss>() << " s\n\n";
-	}
-	file.close();
+    ofstream file;
+    file.open("results.txt");
+    size_t num_passes = 3;
+    for (size_t i = 0; i < num_passes; i++)
+    {
+        file << "Pass " << i << "\n";
+        file << "mersenne:\t" << benchmarkPRNG<mt19937>() << " s\n";
+        file << "mersenne64:\t" << benchmarkPRNG<mt19937_64>() << " s\n";
+        file << "xoroshiro128p:\t" << benchmarkPRNG<xoroshiro128p>() << " s\n";
+        file << "xoshiro256p:\t" << benchmarkPRNG<xoshiro256p>() << " s\n";
+        file << "xoshiro128p:\t" << benchmarkPRNG<xoshiro128p>() << " s\n";
+        file << "xoshiro256ss:\t" << benchmarkPRNG<xoshiro256ss>() << " s\n";
+        file << "xoshiro128ss:\t" << benchmarkPRNG<xoshiro128ss>() << " s\n\n";
+    }
+    file.close();
 
-	return 0;
+    return 0;
 }
--- src/xorshift.h
+++ src/xorshift.h
@@ -17,315 +17,315 @@
 
 namespace xorshift
 {
-	/**
-	 * Splitmix64 PRNG adapted from https://prng.di.unimi.it/splitmix64.c
-	 * Generates 64-bit numbers, only used for the initialization of the other PRNGs.
-	 * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
-	 */
-	class splitmix64
-	{
-	public:
-		using result_type = uint_fast64_t;
-		using state_type = uint_fast64_t;
+    /**
+     * Splitmix64 PRNG adapted from https://prng.di.unimi.it/splitmix64.c
+     * Generates 64-bit numbers, only used for the initialization of the other PRNGs.
+     * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
+     */
+    class splitmix64
+    {
+    public:
+        using result_type = uint_fast64_t;
+        using state_type = uint_fast64_t;
 
-		splitmix64(state_type seed): state(seed) {}
+        splitmix64(state_type seed): state(seed) {}
 
-		result_type operator()() noexcept
-		{
-			state += 0x9e3779b97f4a7c15;
-			result_type z = state;
-			z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
-			z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
-			return z ^ (z >> 31);
-		}
+        result_type operator()() noexcept
+        {
+            state += 0x9e3779b97f4a7c15;
+            result_type z = state;
+            z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+            z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+            return z ^ (z >> 31);
+        }
 
-		static constexpr result_type min() noexcept
-		{
-			return std::numeric_limits<result_type>::min();
-		}
-		static constexpr result_type max() noexcept
-		{
-			return std::numeric_limits<result_type>::max();
-		}
+        static constexpr result_type min() noexcept
+        {
+            return std::numeric_limits<result_type>::min();
+        }
+        static constexpr result_type max() noexcept
+        {
+            return std::numeric_limits<result_type>::max();
+        }
 
-	private:
-		state_type state;
-	};
+    private:
+        state_type state;
+    };
 
-	/**
-	 * xoroshiro128+ PRNG adapted from https://prng.di.unimi.it/xoroshiro128plus.c
-	 * Generates 64-bit numbers. 16 byte state.
-	 * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
-	 */
-	class xoroshiro128p
-	{
-	public:
-		using result_type = uint_fast64_t;
-		using state_type = uint_fast64_t;
+    /**
+     * xoroshiro128+ PRNG adapted from https://prng.di.unimi.it/xoroshiro128plus.c
+     * Generates 64-bit numbers. 16 byte state.
+     * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
+     */
+    class xoroshiro128p
+    {
+    public:
+        using result_type = uint_fast64_t;
+        using state_type = uint_fast64_t;
 
-		xoroshiro128p(uint_fast64_t seed)
-		{
-			splitmix64 seed_seq_gen(seed);
-			state[0] = seed_seq_gen();
-			state[1] = seed_seq_gen();
-		}
+        xoroshiro128p(uint_fast64_t seed)
+        {
+            splitmix64 seed_seq_gen(seed);
+            state[0] = seed_seq_gen();
+            state[1] = seed_seq_gen();
+        }
 
-		xoroshiro128p(std::array<state_type, 2> state) : state(state) {}
+        xoroshiro128p(std::array<state_type, 2> state) : state(state) {}
 
-		result_type operator()() noexcept
-		{
-			const state_type s0 = state[0];
-			state_type s1 = state[1];
-			const result_type result = s0 + s1;
+        result_type operator()() noexcept
+        {
+            const state_type s0 = state[0];
+            state_type s1 = state[1];
+            const result_type result = s0 + s1;
 
-			s1 ^= s0;
-			state[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16);
-			state[1] = rotl(s1, 37);
+            s1 ^= s0;
+            state[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16);
+            state[1] = rotl(s1, 37);
 
-			return result;
-		}
+            return result;
+        }
 
-		static constexpr result_type min() noexcept
-		{
-			return std::numeric_limits<result_type>::min();
-		}
-		static constexpr result_type max() noexcept
-		{
-			return std::numeric_limits<result_type>::max();
-		}
+        static constexpr result_type min() noexcept
+        {
+            return std::numeric_limits<result_type>::min();
+        }
+        static constexpr result_type max() noexcept
+        {
+            return std::numeric_limits<result_type>::max();
+        }
 
-	private:
-		std::array<state_type, 2> state;
+    private:
+        std::array<state_type, 2> state;
 
-		static state_type rotl(state_type x, int k) noexcept
-		{
-			return (x << k) | (x >> (64 - k));
-		}
-	};
+        static state_type rotl(state_type x, int k) noexcept
+        {
+            return (x << k) | (x >> (64 - k));
+        }
+    };
 
-	/**
-	 * xoshiro256+ PRNG adapted from https://prng.di.unimi.it/xoshiro256plus.c
-	 * Generates 64-bit numbers. 32 byte state.
-	 * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
-	 */
-	class xoshiro256p
-	{
-	public:
-		using result_type = uint_fast64_t;
-		using state_type = uint_fast64_t;
+    /**
+     * xoshiro256+ PRNG adapted from https://prng.di.unimi.it/xoshiro256plus.c
+     * Generates 64-bit numbers. 32 byte state.
+     * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
+     */
+    class xoshiro256p
+    {
+    public:
+        using result_type = uint_fast64_t;
+        using state_type = uint_fast64_t;
 
-		xoshiro256p(uint_fast64_t seed)
-		{
-			splitmix64 seed_seq_gen(seed);
-			for(auto& s : state)
-			{
-				s = seed_seq_gen();
-			}
-		}
+        xoshiro256p(uint_fast64_t seed)
+        {
+            splitmix64 seed_seq_gen(seed);
+            for(auto& s : state)
+            {
+                s = seed_seq_gen();
+            }
+        }
 
-		xoshiro256p(std::array<state_type, 4> state) : state(state) {}
+        xoshiro256p(std::array<state_type, 4> state) : state(state) {}
 
-		result_type operator()() noexcept
-		{
-			const result_type result = state[0] + state[3];
-			const state_type t = state[1] << 17;
+        result_type operator()() noexcept
+        {
+            const result_type result = state[0] + state[3];
+            const state_type t = state[1] << 17;
 
-			state[2] ^= state[0];
-			state[3] ^= state[1];
-			state[1] ^= state[2];
-			state[0] ^= state[3];
+            state[2] ^= state[0];
+            state[3] ^= state[1];
+            state[1] ^= state[2];
+            state[0] ^= state[3];
 
-			state[2] ^= t;
-			state[3] = rotl(state[3], 45);
+            state[2] ^= t;
+            state[3] = rotl(state[3], 45);
 
-			return result;
-		}
+            return result;
+        }
 
-		static constexpr result_type min() noexcept
-		{
-			return std::numeric_limits<result_type>::min();
-		}
-		static constexpr result_type max() noexcept
-		{
-			return std::numeric_limits<result_type>::max();
-		}
+        static constexpr result_type min() noexcept
+        {
+            return std::numeric_limits<result_type>::min();
+        }
+        static constexpr result_type max() noexcept
+        {
+            return std::numeric_limits<result_type>::max();
+        }
 
-	private:
-		std::array<state_type, 4> state;
+    private:
+        std::array<state_type, 4> state;
 
-		static state_type rotl(state_type x, int k) noexcept
-		{
-			return (x << k) | (x >> (64 - k));
-		}
-	};
+        static state_type rotl(state_type x, int k) noexcept
+        {
+            return (x << k) | (x >> (64 - k));
+        }
+    };
 
-	/**
-	 * xoshiro128+ PRNG adapted from https://prng.di.unimi.it/xoshiro128plus.c
-	 * Generates 32-bit numbers. 16 byte state.
-	 * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
-	 */
-	class xoshiro128p
-	{
-	public:
-		using result_type = uint_fast64_t;
-		using state_type = uint_fast64_t;
+    /**
+     * xoshiro128+ PRNG adapted from https://prng.di.unimi.it/xoshiro128plus.c
+     * Generates 32-bit numbers. 16 byte state.
+     * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
+     */
+    class xoshiro128p
+    {
+    public:
+        using result_type = uint_fast64_t;
+        using state_type = uint_fast64_t;
 
-		xoshiro128p(uint_fast64_t seed)
-		{
-			splitmix64 seed_seq_gen(seed);
-			for(auto& s : state)
-			{
-				s = state_type(seed_seq_gen());
-			}
-		}
+        xoshiro128p(uint_fast64_t seed)
+        {
+            splitmix64 seed_seq_gen(seed);
+            for(auto& s : state)
+            {
+                s = state_type(seed_seq_gen());
+            }
+        }
 
-		xoshiro128p(std::array<state_type, 4> state) : state(state) {}
+        xoshiro128p(std::array<state_type, 4> state) : state(state) {}
 
-		result_type operator()() noexcept
-		{
-			const result_type result = state[0] + state[3];
-			const state_type t = state[1] << 9;
+        result_type operator()() noexcept
+        {
+            const result_type result = state[0] + state[3];
+            const state_type t = state[1] << 9;
 
-			state[2] ^= state[0];
-			state[3] ^= state[1];
-			state[1] ^= state[2];
-			state[0] ^= state[3];
+            state[2] ^= state[0];
+            state[3] ^= state[1];
+            state[1] ^= state[2];
+            state[0] ^= state[3];
 
-			state[2] ^= t;
-			state[3] = rotl(state[3], 11);
+            state[2] ^= t;
+            state[3] = rotl(state[3], 11);
 
-			return result;
-		}
+            return result;
+        }
 
-		static constexpr result_type min() noexcept
-		{
-			return std::numeric_limits<result_type>::min();
-		}
-		static constexpr result_type max() noexcept
-		{
-			return std::numeric_limits<result_type>::max();
-		}
+        static constexpr result_type min() noexcept
+        {
+            return std::numeric_limits<result_type>::min();
+        }
+        static constexpr result_type max() noexcept
+        {
+            return std::numeric_limits<result_type>::max();
+        }
 
-	private:
-		std::array<state_type, 4> state;
+    private:
+        std::array<state_type, 4> state;
 
-		static state_type rotl(state_type x, int k) noexcept
-		{
-			return (x << k) | (x >> (32 - k));
-		}
-	};
+        static state_type rotl(state_type x, int k) noexcept
+        {
+            return (x << k) | (x >> (32 - k));
+        }
+    };
 
-	/**
-	 * xoshiro256** PRNG adapted from https://prng.di.unimi.it/xoshiro256starstar.c
-	 * Generates 64-bit numbers. 32 byte state. Highest quality 64 bit generator.
-	 * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
-	 */
-	class xoshiro256ss
-	{
-	public:
-		using result_type = uint_fast64_t;
-		using state_type = uint_fast64_t;
+    /**
+     * xoshiro256** PRNG adapted from https://prng.di.unimi.it/xoshiro256starstar.c
+     * Generates 64-bit numbers. 32 byte state. Highest quality 64 bit generator.
+     * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
+     */
+    class xoshiro256ss
+    {
+    public:
+        using result_type = uint_fast64_t;
+        using state_type = uint_fast64_t;
 
-		xoshiro256ss(uint_fast64_t seed)
-		{
-			splitmix64 seed_seq_gen(seed);
-			for(auto& s : state)
-			{
-				s = seed_seq_gen();
-			}
-		}
+        xoshiro256ss(uint_fast64_t seed)
+        {
+            splitmix64 seed_seq_gen(seed);
+            for(auto& s : state)
+            {
+                s = seed_seq_gen();
+            }
+        }
 
-		xoshiro256ss(std::array<state_type, 4> state) : state(state) {}
+        xoshiro256ss(std::array<state_type, 4> state) : state(state) {}
 
-		result_type operator()() noexcept
-		{
-			const result_type result = rotl(state[1] * 5, 7) * 9;
-			const state_type t = state[1] << 17;
+        result_type operator()() noexcept
+        {
+            const result_type result = rotl(state[1] * 5, 7) * 9;
+            const state_type t = state[1] << 17;
 
-			state[2] ^= state[0];
-			state[3] ^= state[1];
-			state[1] ^= state[2];
-			state[0] ^= state[3];
+            state[2] ^= state[0];
+            state[3] ^= state[1];
+            state[1] ^= state[2];
+            state[0] ^= state[3];
 
-			state[2] ^= t;
-			state[3] = rotl(state[3], 45);
+            state[2] ^= t;
+            state[3] = rotl(state[3], 45);
 
-			return result;
-		}
+            return result;
+        }
 
-		static constexpr result_type min() noexcept
-		{
-			return std::numeric_limits<result_type>::min();
-		}
-		static constexpr result_type max() noexcept
-		{
-			return std::numeric_limits<result_type>::max();
-		}
+        static constexpr result_type min() noexcept
+        {
+            return std::numeric_limits<result_type>::min();
+        }
+        static constexpr result_type max() noexcept
+        {
+            return std::numeric_limits<result_type>::max();
+        }
 
-	private:
-		std::array<state_type, 4> state;
+    private:
+        std::array<state_type, 4> state;
 
-		static state_type rotl(state_type x, int k) noexcept
-		{
-			return (x << k) | (x >> (64 - k));
-		}
-	};
+        static state_type rotl(state_type x, int k) noexcept
+        {
+            return (x << k) | (x >> (64 - k));
+        }
+    };
 
-	/**
-	 * xoshiro128** PRNG adapted from 
-	 * Generates 32-bit numbers. 16 byte state. Highest quality 32 bit generator.
-	 * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
-	 */
-	class xoshiro128ss
-	{
-	public:
-		using result_type = uint_fast32_t;
-		using state_type = uint_fast32_t;
-		
-		xoshiro128ss(uint_fast64_t seed)
-		{
-			splitmix64 seed_seq_gen(seed);
-			for(auto& s : state)
-			{
-				s = state_type(seed_seq_gen());
-			}
-		}
+    /**
+     * xoshiro128** PRNG adapted from 
+     * Generates 32-bit numbers. 16 byte state. Highest quality 32 bit generator.
+     * Works with the standard library distributions, qualifies as std::uniform_random_bit_generator
+     */
+    class xoshiro128ss
+    {
+    public:
+        using result_type = uint_fast32_t;
+        using state_type = uint_fast32_t;
+        
+        xoshiro128ss(uint_fast64_t seed)
+        {
+            splitmix64 seed_seq_gen(seed);
+            for(auto& s : state)
+            {
+                s = state_type(seed_seq_gen());
+            }
+        }
 
-		xoshiro128ss(std::array<state_type, 4> state) : state(state) {}
+        xoshiro128ss(std::array<state_type, 4> state) : state(state) {}
 
-		result_type operator()() noexcept
-		{
-			const result_type result = rotl(state[1] * 5, 7) * 9;
-			const state_type t = state[1] << 9;
+        result_type operator()() noexcept
+        {
+            const result_type result = rotl(state[1] * 5, 7) * 9;
+            const state_type t = state[1] << 9;
 
-			state[2] ^= state[0];
-			state[3] ^= state[1];
-			state[1] ^= state[2];
-			state[0] ^= state[3];
+            state[2] ^= state[0];
+            state[3] ^= state[1];
+            state[1] ^= state[2];
+            state[0] ^= state[3];
 
-			state[2] ^= t;
-			state[3] = rotl(state[3], 11);
+            state[2] ^= t;
+            state[3] = rotl(state[3], 11);
 
-			return result;
-		}
-		
-		static constexpr result_type min() noexcept	
-		{
-			return std::numeric_limits<result_type>::min();
-		}
-		static constexpr result_type max() noexcept
-		{
-			return std::numeric_limits<result_type>::max();
-		}
+            return result;
+        }
+        
+        static constexpr result_type min() noexcept    
+        {
+            return std::numeric_limits<result_type>::min();
+        }
+        static constexpr result_type max() noexcept
+        {
+            return std::numeric_limits<result_type>::max();
+        }
 
-	private:
-		std::array<state_type, 4> state;
+    private:
+        std::array<state_type, 4> state;
 
-		static state_type rotl(state_type x, int k) noexcept
-		{
-			return (x << k) | (x >> (32 - k));
-		}
-	};
+        static state_type rotl(state_type x, int k) noexcept
+        {
+            return (x << k) | (x >> (32 - k));
+        }
+    };
 
 } // namespace xorshift
 
