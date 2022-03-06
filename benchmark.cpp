f7f041274a06a786aa9ac1ab01088afc82eff0a0
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-03-06 12:14:41 +0100

    updated benchmarks

--- benchmark.cpp
+++ benchmark.cpp
@@ -3,56 +3,55 @@
 #include <chrono>
 #include <vector>
 #include <algorithm>
+#include <benchmark/benchmark.h>
 
-#include "src/xorshift.h"
+#include "src/xorshift.hpp"
 
 using namespace std;
 using namespace xorshift;
 
 template<typename PRNG>
-double benchmarkPRNG(size_t num_rands = 50'000'000)
+static void DBL(benchmark::State& state)
 {
-    PRNG gen{ random_device{}() };
-    uniform_real_distribution<double> dist(0.0, 100.0);
-    vector<double> numbers(num_rands);
+  PRNG gen{ random_device{}() };
+  uniform_real_distribution<double> dist(0.0, 1.0);
 
-    double running_mean_time = 0.0;
-    /* Run 5 times and take the average time. */
-    for (size_t i = 1; i <= 5; i++)
-    {
-        auto tbegin = chrono::high_resolution_clock::now();
-        generate(numbers.begin(), numbers.end(),
-        [&gen, &dist]()
-        {
-            return dist(gen);
-        });
-        auto tend = chrono::high_resolution_clock::now();
-
-        auto duration = chrono::duration_cast<chrono::microseconds>(tend - tbegin).count();
-        double t = double(duration) / 1E+6;
-        running_mean_time += (t - running_mean_time) / i;
-    }
-
-    return running_mean_time;
+  for (auto _ : state)
+  {
+      benchmark::DoNotOptimize(dist(gen));
+  }
 }
 
-int main()
+template<typename PRNG>
+static void INT(benchmark::State& state)
 {
-    ofstream file;
-    file.open("results.txt");
-    size_t num_passes = 3;
-    for (size_t i = 0; i < num_passes; i++)
-    {
-        file << "Pass " << i << "\n";
-        file << "mersenne:\t" << benchmarkPRNG<mt19937>() << " s\n";
-        file << "mersenne64:\t" << benchmarkPRNG<mt19937_64>() << " s\n";
-        file << "xoroshiro128p:\t" << benchmarkPRNG<xoroshiro128p>() << " s\n";
-        file << "xoshiro256p:\t" << benchmarkPRNG<xoshiro256p>() << " s\n";
-        file << "xoshiro128p:\t" << benchmarkPRNG<xoshiro128p>() << " s\n";
-        file << "xoshiro256ss:\t" << benchmarkPRNG<xoshiro256ss>() << " s\n";
-        file << "xoshiro128ss:\t" << benchmarkPRNG<xoshiro128ss>() << " s\n\n";
-    }
-    file.close();
+  PRNG gen{ random_device{}() };
+  uniform_int_distribution<int> dist(0, 100);
 
-    return 0;
+  for (auto _ : state)
+  {
+      benchmark::DoNotOptimize(dist(gen));
+  }
 }
+
+BENCHMARK_TEMPLATE(DBL, mt19937);
+BENCHMARK_TEMPLATE(DBL, mt19937_64);
+BENCHMARK_TEMPLATE(DBL, ranlux24_base);
+BENCHMARK_TEMPLATE(DBL, ranlux48_base);
+BENCHMARK_TEMPLATE(DBL, xoroshiro128p);
+BENCHMARK_TEMPLATE(DBL, xoshiro256p);
+BENCHMARK_TEMPLATE(DBL, xoshiro128p);
+BENCHMARK_TEMPLATE(DBL, xoshiro256ss);
+BENCHMARK_TEMPLATE(DBL, xoshiro128ss);
+
+BENCHMARK_TEMPLATE(INT, mt19937);
+BENCHMARK_TEMPLATE(INT, mt19937_64);
+BENCHMARK_TEMPLATE(INT, ranlux24_base);
+BENCHMARK_TEMPLATE(INT, ranlux48_base);
+BENCHMARK_TEMPLATE(INT, xoroshiro128p);
+BENCHMARK_TEMPLATE(INT, xoshiro256p);
+BENCHMARK_TEMPLATE(INT, xoshiro128p);
+BENCHMARK_TEMPLATE(INT, xoshiro256ss);
+BENCHMARK_TEMPLATE(INT, xoshiro128ss);
+
+BENCHMARK_MAIN();
